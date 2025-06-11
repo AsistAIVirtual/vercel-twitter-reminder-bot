c77ac04cd6e89c4d0107aa5559da457e39500f7a
AsistAI <asistaivirtuals@gmail.com>
2025-06-11 03:29:04 +0300

    Initial commit

---
+++ api/subscribe-tweet.js
@@ -0,0 +1,30 @@
+import { TwitterApi } from 'twitter-api-v2';
+
+const client = new TwitterApi({
+  appKey: process.env.TWITTER_API_KEY,
+  appSecret: process.env.TWITTER_API_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_SECRET,
+});
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Only POST requests allowed' });
+  }
+
+  const { twitterUsername, tokenName, days } = req.body;
+
+  if (!twitterUsername || !tokenName || !days) {
+    return res.status(400).json({ error: 'Missing parameters' });
+  }
+
+  const message = `@${twitterUsername} Your reminder has been recorded. You'll be notified ${days} days before the unlock of token ${tokenName}.`;
+
+  try {
+    await client.v2.tweet(message);
+    res.status(200).json({ success: true, message });
+  } catch (err) {
+    console.error('Tweet error:', err);
+    res.status(500).json({ error: 'Failed to send tweet' });
+  }
+}
